084ba738ace41c18aec6927953d809de83adad22
Raimer Thompson <rthomp22@gmail.com>
2023-12-14 17:35:20 -0500

    added temp converter app

---
+++ tempConverter/temp.js
@@ -0,0 +1,17 @@
+document.querySelector('submit').addEventListener('click', Convertor)
+
+  // total = (total * (9/5) + 32);
+
+
+function Convertor(){
+//get input value for celcicus
+
+
+
+// convert value into fahrenheit
+
+//show it on screen
+}
+ 
+
+ 
