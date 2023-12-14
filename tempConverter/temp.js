2bd00d17c5656b6c1575e234545ddc670f429ad0
Raimer Thompson <rthomp22@gmail.com>
2023-12-14 17:39:50 -0500

    added functionality js

--- tempConverter/temp.js
+++ tempConverter/temp.js
@@ -1,4 +1,4 @@
-document.querySelector('submit').addEventListener('click', Convertor)
+document.querySelector('#submit').addEventListener('click', Convertor)
 
   // total = (total * (9/5) + 32);
 
@@ -6,11 +6,15 @@ document.querySelector('submit').addEventListener('click', Convertor)
 function Convertor(){
 //get input value for celcicus
 
-
+let temp = document.querySelector('#input').value
 
 // convert value into fahrenheit
 
+temp = temp * 9/5 + 32;
+
 //show it on screen
+
+document.querySelector('#total').innerText = temp
 }
  
 
