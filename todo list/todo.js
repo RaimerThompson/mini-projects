7013adff647426ee5b5171cd7dc3faed7f607961
Raimer Thompson <rthomp22@gmail.com>
2023-12-20 05:37:15 -0500

    new array project added

--- todo list/todo.js
+++ todo list/todo.js
@@ -3,16 +3,22 @@ let addBtn = document.querySelector('.add')
 let todoContainer = document.querySelector('.todos')
 
 
-
+//grab the add btn and added event listner 
 addBtn.addEventListener('click', function(){
+  //created the todo element paragraph
   let paragraph = document.createElement('p')
   paragraph.classList.add('style')
+  //assign the input value to the new p innertext
   paragraph.innerText = input.value
+  //emptied the input value to allow to add new value
   input.value= ''
+  // added the new paragraph element
   todoContainer.appendChild(paragraph)
+  //added click function to new p element to add the style line-through
   paragraph.addEventListener('click', function (){
     paragraph.style.textDecoration= 'line-through'
   })
+  // added a double click to  and remove the p elemtn todo
   paragraph.addEventListener('dblclick', function (){
     todoContainer.removeChild(paragraph)
   })
