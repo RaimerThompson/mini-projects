8415fe817ff729748d439693990590455b8697a2
Raimer Thompson <rthomp22@gmail.com>
2023-12-21 18:28:36 -0500

    added arrays to to list inputs

---
+++ arrays/arr.js
@@ -0,0 +1,35 @@
+function multy(arr){
+  let product = 1
+
+  arr.forEach(num => product *= num)
+  alert(product)
+}
+
+multy([10,2,3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let myarray = [1,2,3,4]
+
+// function compare(myarray){
+//   if (myarray[0]< myarray[myarray.length-1]){
+//     alert('Hi')
+//   }else if (myarray[0]> myarray[myarray.length-1]){
+//     alert('Bye')
+//   }else if (myarray[0] === myarray[myarray.length-1]){
+//     alert('We close in an hour')
+//   }
+// }
+
+// let nums = [4, 3,5,6,7,22]
+// compare(nums)
--- todo list/todo.js
+++ todo list/todo.js
@@ -2,14 +2,21 @@ let input = document.querySelector('.input')
 let addBtn = document.querySelector('.add')
 let todoContainer = document.querySelector('.todos')
 
+const todoList = []
+
 
 //grab the add btn and added event listner 
 addBtn.addEventListener('click', function(){
   //created the todo element paragraph
   let paragraph = document.createElement('p')
-  paragraph.classList.add('style')
+  // paragraph.classList.add('style')
   //assign the input value to the new p innertext
-  paragraph.innerText = input.value
+  let name = input.value
+  todoList.push(name) 
+   paragraph.innerText = todoList
+   console.log(todoList);
+  
+  // paragraph.innerText = input.value
   //emptied the input value to allow to add new value
   input.value= ''
   // added the new paragraph element
@@ -21,5 +28,7 @@ addBtn.addEventListener('click', function(){
   // added a double click to  and remove the p elemtn todo
   paragraph.addEventListener('dblclick', function (){
     todoContainer.removeChild(paragraph)
+    todoList.pop(name)
+    console.log(todoList);
   })
 })
